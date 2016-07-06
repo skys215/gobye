fb217044c8f19b90da6fe8703d0bbfc6e14200fa
Pencil <9807175@qq.com>
2016-07-06 10:55:29 +0800

    修复已知bug
    修复体育学分bug, 课程名去除英文名。修改了课程匹配的方式。

--- datadeal.py
+++ datadeal.py
@@ -27,32 +27,37 @@ def getIndex(string):
     elif string == '学科专业选修课':
         return 3
 
+
 def DivedeAll(result):
     for i in range(0,len(result)):
         result[i] = divideInfo(result[i])
 
+#将数据分割为数组
 def divideInfo(Info):
     result_temp = Info.split('  ')
     if len(result_temp)<11:#正常情况下会被分割成11或12个
         result_temp = Info.split('\t')#目前发现有可能以\t分隔
         if len(result_temp)<11:
             return 0
-        result_temp[6] = float(result_temp[6])
+    result_temp[6] = float(result_temp[6])
         #其他情况先不处理...
     return result_temp
 
+#查找挂科科目,并统计已修学分
 def getFailCourse(result_now, result_pre,resultFail):
     score = [0.0,0.0,0.0,0.0]
     for x in range(0,len(result_pre)):
         if result_pre[x]!=0:
             if result_pre[x][6]=='0':#第7个为取得学分,如果为0表示挂科
-                resultFail.append([result_pre[x][3],result_pre[x][4],result_pre[x][5]])
+                resultFail.append([result_pre[x][3],result_pre[x][4],result_pre[x][5],result_pre[x][2]])
             else :
                 #判断挂科科目是否重修通过
                 for fail in resultFail:
                     if fail[0]==result_pre[x][3] and fail[1]==result_pre[x][4] and fail[2]==result_pre[x][5]:
                         resultFail.remove(fail)
-            score[getIndex(result_pre[x][4])] += float(result_pre[x][6])
+                score[getIndex(result_pre[x][4])] += float(result_pre[x][6])
+   
+    #统计最新选课结果的学分,并查找是否重修挂科科目
     for x in range(0,len(result_now)):
         if result_now[x]!=0:
             #判断挂科科目是否重修
@@ -79,32 +84,97 @@ def getAllCource(all_text,result_now,result_pre):
     loc_old = all_text.find('学分:')#最新结果的冒号为全角
 
     loc_comment =  all_text.find('备注')
-    # if loc_now<loc_old:
+    if loc_now<loc_old:
         #添加最新结果
-    result_list = getCource(all_text,loc_comment,1)
-    for x in range(1,len(result_list[0])-1):
-        if result_list[0][x].replace(' ', '')!='':
-            result_now.append(result_list[0][x])
-    #添加以前的选课结果
-    location = all_text.find('备注',result_list[1])
-    while location!=-1:
-        result_list = getCource(all_text,location,0)
+        result_list = getCource(all_text,loc_comment,1)
         for x in range(1,len(result_list[0])-1):
             if result_list[0][x].replace(' ', '')!='':
-                result_pre.append(result_list[0][x])
+                result_now.append(result_list[0][x])
+        #添加以前的选课结果
         location = all_text.find('备注',result_list[1])
+        while location!=-1:
+            result_list = getCource(all_text,location,0)
+            for x in range(1,len(result_list[0])-1):
+                if result_list[0][x].replace(' ', '')!='':
+                    result_pre.append(result_list[0][x])
+            location = all_text.find('备注',result_list[1])
+    else :
+        #添加以前的选课结果
+        location = all_text.find('备注',loc_comment)
+        while loc_old!=-1:
+            result_list = getCource(all_text,location,0)
+            for x in range(1,len(result_list[0])-1):
+                if result_list[0][x].replace(' ', '')!='':
+                    result_pre.append(result_list[0][x])
+            location = all_text.find('备注',result_list[1])
+            loc_old = all_text.find('学分:',location)
+            #序号后面找不到'学分:'则说明以前的选课结果已经获取完毕
+        #添加最新结果
+        result_list = getCource(all_text,location,1)
+        for x in range(1,len(result_list[0])-1):
+            if result_list[0][x].replace(' ', '')!='':
+                result_now.append(result_list[0][x])
 
-def myFind(cource_name,source,index):#查找cource_name是否有包含source列表中的第index项
-    for x in source:
-        if cource_name.find(x[index])!=-1:
-            return 1
+    ################一下注释代码为不判断粘贴顺序#########
+    # loc_comment =  all_text.find('备注')
+    # # if loc_now<loc_old:
+    #     #添加最新结果
+    # result_list = getCource(all_text,loc_comment,1)
+    # for x in range(1,len(result_list[0])-1):
+    #     if result_list[0][x].replace(' ', '')!='':
+    #         result_now.append(result_list[0][x])
+    # #添加以前的选课结果
+    # location = all_text.find('备注',result_list[1])
+    # while location!=-1:
+    #     result_list = getCource(all_text,location,0)
+    #     for x in range(1,len(result_list[0])-1):
+    #         if result_list[0][x].replace(' ', '')!='':
+    #             result_pre.append(result_list[0][x])
+    #     location = all_text.find('备注',result_list[1])
+
+#判断课程(course_name,course_num)是否已修,已修返回1
+def myFind(course_name, course_num,result_now, result_pre, result_fail):
+    if course_name.find("体育俱乐部")!=-1:#体育学分特殊处理
+        for x in result_now:
+            if x[5].find("俱乐部")!=-1:
+                return 1
+        for x in result_pre:
+            if x[3].find("俱乐部")!=-1:
+                return 1
+        for x in result_fail:
+            if x[0].find("俱乐部")==-1:
+                return 1
+    else :
+        #通过对比课程号和课程名来匹配课程。
+        for x in result_now:
+            if x[2].find(course_num)!=-1 or course_name.find(x[5])!=-1:
+                return 1
+        for x in result_pre:
+            if x[2].find(course_num)!=-1 or course_name.find(x[3])!=-1:
+                return 1
+        for x in result_fail:
+            if x[3].find(course_num)!=-1 or course_name.find(x[0])==-1:
+                return 1
     return 0
+
 def updateType(result,profess_id,index):
     for i in range(0,len(result)):
         iget = plan.objects.filter(Q(profess_id=profess_id)).filter(course_name__contains=str(result[i][index]))
         a = iget
         if a:
             result[i][4] = a[0].course_type
+
+#去除课程的英文名。
+def Format(find_cource):
+    alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', ',Z']
+    for x in find_cource:
+        x.credit = float(x.credit)
+        i = 999
+        for a in alpha:
+            j = x.course_name.find(a)
+            if j!=-1 and j<i and x.course_name[j:].count(')')<2:
+                i = j
+        x.course_name = x.course_name[:i]
 
 def Compare(stu_major,stu_grade,result_now,result_pre,result_fail,creditNeed):
     iget = professes.objects.filter(year = stu_grade).filter(profess = stu_major)
@@ -124,21 +194,24 @@ def Compare(stu_major,stu_grade,result_now,result_pre,result_fail,creditNeed):
     creditNeed[3] = float(iget[2].course_need[iget[2].course_need.find("要求")+2:iget[2].course_need.find("学分")])
     creditNeed[1] = float(iget[2].course_grade)-creditNeed[3]
 
-    find_cource=[]
-    iget = plan.objects.filter(profess_id=str(profess_id)).filter(Q(course_type='公 共必修课')|Q(course_type='学科专业核心课'))
-    a = iget
-
+    find_course=[]
+    iget = plan.objects.filter(profess_id=str(profess_id)).filter(Q(course_type='公共必修课')|Q(course_type='学科专业核心课'))
+    a = list(iget)
+    for x in a:
+        find_course.append(x)
+    
     updateType(result_now,profess_id,5)#更新已修课程的课程类型
     updateType(result_pre,profess_id,3)
 
     i = 0
     j = 0
     for x in a:
-       if not((myFind(x.course_name,result_now,5)==1 
-        or myFind(x.course_name,result_pre,3)==1) 
-        and myFind(x.course_name,result_fail,0)==0):
-            find_cource.append(x)
-    return find_cource
+        if myFind(x.course_name,x.total_number, result_now, result_pre, result_fail)==1:
+            find_course.remove(x)
+    
+    Format(find_course)
+
+    return find_course
 
 def Sort(CourseP, CoursePE, CourseM, CourseME, source, index, flag):
     for x in source:
--- views.py
+++ views.py
@@ -23,8 +23,7 @@ def result(request):
     all_text = delSpace(all_text)
 
     if all_text.find(u'学号')==-1 or all_text.find(u'主修专业')==-1:
-        #找不到学号或主修专业
-        print "error1"
+        return HttpResponse("找不到学号或主修专业")
 
     #获取专业及年纪
     stu_num = all_text[all_text.find(u'学号')+3:all_text.find(u'姓名')-2]
@@ -33,57 +32,61 @@ def result(request):
     while stu_major.find(' ')!=-1:
         stu_major = stu_major.replace(' ','')
 
-    #获取所有选课结果
-    result_pre = []#记录以前的成绩
-    result_now = []#记录本学期选课
-    getAllCource(all_text,result_now,result_pre)
+    try:
+        #获取所有选课结果
+        result_pre = []#记录以前的成绩
+        result_now = []#记录本学期选课
+        getAllCource(all_text,result_now,result_pre)
+        DivedeAll(result_now)
+        DivedeAll(result_pre)
 
-    DivedeAll(result_now)
-    DivedeAll(result_pre)
+        #分析以前的成绩,判断挂科科目
+        result_fail = []#储存挂科科目.注:result_fail[0]为课程名,result_fail[1]为选修或必修,2为学分
+        creditNeed = [0.0,0.0,0.0,0.0]
 
-    #分析以前的成绩,判断挂科科目
-    result_fail = []#储存挂科科目.注:result_fail[0]为课程名,result_fail[1]为选修或必修,2为学分
-    creditNeed = [0.0,0.0,0.0,0.0]
+        #与培养方案比较，计算需修学分
 
-    if len(result_fail)>0:
-        print "挂科科目:"
-        for i in result_fail:
-            print "课程名:"+i[0]+"    学分类型:"+i[1]+"    学分:"+i[2]
+        final_result = Compare(stu_major,stu_grade,result_now,result_pre,result_fail,creditNeed)
 
-    #与培养方案比较，计算需修学分
+        creditGet = getFailCourse(result_now, result_pre,result_fail)#creditGet[0]为必修已获取学分,creditGet[1]为选修已获取学分
+        #区分公共必修，专业必修
+        CourseP = []#公共必修
+        CourseM = []#专业必修
+        CoursePE = []#公共选修
+        CourseME = []#专业选修
 
-    final_result = Compare(stu_major,stu_grade,result_now,result_pre,result_fail,creditNeed)
+        if final_result==0:
+            print "error2"
+        else :
+            #print "主修专业:"+stu_major
+            #print "年级:"+stu_grade
+            #print "已修公共必修学分:"+str(creditGet[0])+"    已修公共选修学分:"+str(creditGet[1])+"      已修专业必修学分:"+str(creditGet[2])+"    已修专业选修学分:"+str(creditGet[3])
+            #print "需修公共必修学分:"+str(creditNeed[0])+"    需修公共选修学分:"+str(creditNeed[1])+"      需修专业必修学分:"+str(creditNeed[2])+"    需修专业选修学分:"+str(creditNeed[3])
+            result_final = []
+            for each_final in final_result:
+                each_result = []
+                each_result.append(each_final.id)
+                each_result.append(each_final.profess_id)
+                each_result.append(each_final.course_name)
+                each_result.append(each_final.total_number)
+                each_result.append(each_final.credit)
+                each_result.append(each_final.credit_type)
+                each_result.append(each_final.course_type)
+                result_final.append(each_result)
+                
+            Sort(CourseP, CoursePE, CourseM, CourseME, result_final, 6, 0)
+            Sort(CourseP, CoursePE, CourseM, CourseME, result_pre, 4, 1)
+            Sort(CourseP, CoursePE, CourseM, CourseME, result_now, 4, 2)
 
-    creditGet = getFailCourse(result_now, result_pre,result_fail)#creditGet[0]为必修已获取学分,creditGet[1]为选修已获取学分
-    #区分公共必修，专业必修
-    CourseP = []#公共必修
-    CourseM = []#专业必修
-    CoursePE = []#公共选修
-    CourseME = []#专业选修
+        creditLack = [creditNeed[0] - creditGet[0],creditNeed[1] - creditGet[1],creditNeed[2] - creditGet[2],creditNeed[3] - creditGet[3]]
+        lack = creditLack[0]+creditLack[1]+creditLack[2]+creditLack[3]
 
-    if final_result==0:
-        print "error2"
-    else :
-        #print "主修专业:"+stu_major
-        #print "年级:"+stu_grade
-        #print "已修公共必修学分:"+str(creditGet[0])+"    已修公共选修学分:"+str(creditGet[1])+"      已修专业必修学分:"+str(creditGet[2])+"    已修专业选修学分:"+str(creditGet[3])
-        #print "需修公共必修学分:"+str(creditNeed[0])+"    需修公共选修学分:"+str(creditNeed[1])+"      需修专业必修学分:"+str(creditNeed[2])+"    需修专业选修学分:"+str(creditNeed[3])
-        result_final = []
-        for each_final in final_result:
-            each_result = []
-            each_result.append(each_final.id)
-            each_result.append(each_final.profess_id)
-            each_result.append(each_final.course_name)
-            each_result.append(each_final.total_number)
-            each_result.append(each_final.credit)
-            each_result.append(each_final.credit_type)
-            each_result.append(each_final.course_type)
-            result_final.append(each_result)
-            
-        Sort(CourseP, CoursePE, CourseM, CourseME, result_final, 6, 0)
-        Sort(CourseP, CoursePE, CourseM, CourseME, result_pre, 4, 1)
-        Sort(CourseP, CoursePE, CourseM, CourseME, result_now, 4, 2)
+    except :
+        return HttpResponse("Error")
 
-    lack = creditNeed[0] + creditNeed[1] + creditNeed[2] + creditNeed[3] - creditGet[0] - creditGet[1] - creditGet[2] - creditGet[3]
+    # if len(result_fail)>0:
+    #     print "挂科科目:"
+    #     for i in result_fail:
+    #         print "课程名:"+i[0]+"    学分类型:"+i[1]+"    学分:"+i[2]
 
-    return render(request, 'gobye/result.html',{'CourseP':CourseP,'CourseM':CourseM,'CoursePE':CoursePE,'CourseME':CourseME,'creditGet':creditGet,'creditNeed':creditNeed,'lack':lack})
+    return render(request, 'gobye/result.html',{'CourseP':CourseP,'CourseM':CourseM,'CoursePE':CoursePE,'CourseME':CourseME,'creditGet':creditGet,'creditNeed':creditNeed,'lack':lack,'creditLack':creditLack})
